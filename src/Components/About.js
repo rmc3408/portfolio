b0bb8c9dabfc43ee11316e5a8e105e54ba2f3f29
MARTIANSID <59787009+MARTIANSID@users.noreply.github.com>
2020-09-04 00:02:34 +0530

    nav changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,21 +5,39 @@ const About = () => {
   return (
     <div className={classes.about}>
       <div className={classes.about_text}>
-        <div> <h2>About</h2>
-      
-      <p>
-        I'm a logical, critical thinker that enjoys the problem solving and
-        continuous learning inherent in software development. My mission is to
-        use my skills and passion for learning to build intuitive and
-        responsive web applications.
-      </p></div>
-         
+        <div>
+          <h2>About</h2>
+          <p>
+            I'm a logical, critical thinker that enjoys the problem solving and
+            continuous learning inherent in software development. My mission is
+            to use my skills and passion for learning to build intuitive and
+            responsive web applications.
+          </p>
+        </div>
       </div>
       <div className={classes.about_cards}>
-          <div></div>
-          <div></div>
-          <div></div>
-
+        <div>
+          <h3>Langauges</h3>
+          <ul>
+            <li>Javascript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
       </div>
       <div className={classes.project}></div>
     </div>
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -2,12 +2,13 @@ import React from "react";
 import classes from "./navbar.module.css";
 
 const Navbar = () => {
+    const arr=[classes.active,classes.list_item]
   return <nav className={classes.main_nav}>
       <ul className={classes.nav_items}>
-          <li className={classes.active}>Home</li>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Contact</li>
+          <li className={arr.join(" ")}>Home</li>
+          <li className={classes.list_item}>About</li>
+          <li className={classes.list_item}>Projects</li>
+          <li className={classes.list_item}>Contact</li>
       </ul>
 
   </nav>
