6a974dfe6084ca1d204070aa7523c6b277120ffe
rmc3408 <rmc3408@protonmail.com>
2020-09-04 14:17:19 -0400

    a

--- src/Components/imageSection.js
+++ src/Components/imageSection.js
@@ -1,10 +1,18 @@
 import React from "react";
-import classes from './imageSection.module.css'
+import classes from './imageSection.module.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
+AOS.init();
+
 const ImageSection = () => {
   return (
   <div className={classes.imageContainer}>
-      
+      <h1 style={{ color: 'white' }}>Hello, My name is Raphael Molinaro</h1>
+      <h3 style={{ color: 'white' }}>I'm a software Engineer</h3>
   </div>
   )
 };
+
 export default ImageSection;
