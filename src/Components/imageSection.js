25391201e74111efdc5d87681914e7f08b5ae43d
rmc3408 <rmc3408@protonmail.com>
2020-09-04 16:11:27 -0400

    animation ImageSection

--- src/Components/imageSection.js
+++ src/Components/imageSection.js
@@ -4,14 +4,21 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 
-AOS.init();
+AOS.init({
+  duration: 2200
+ });
 
 const ImageSection = () => {
   return (
     <div className={classes.imageContainer}>
-      <div className={classes.boxTitle}>
-        <h1 className={classes.titleName}> Hello, My name is Raphael Molinaro </h1>
-        <h3>I'm a software Engineer</h3>
+      <div className={classes.boxTitle} >
+        <h1 data-aos="fade-right"
+            data-aos-offset="300"
+            data-aos-easing="ease-in-sine"
+            className={classes.titleName}> Hello, My name is Raphael Molinaro </h1>
+        <h3 data-aos="fade-up"
+          data-aos-delay="700"
+          data-aos-duration="3000"> I'm a software Engineer</h3>
       </div>
       
   </div>
