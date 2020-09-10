d3cf990298b020c8c5d232d393ccce68d2d60a2f
rmc3408 <rmc3408@protonmail.com>
2020-09-10 16:17:16 -0400

    version 1.0

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,14 +1,16 @@
 import React from "react";
 import classes from "./Contact.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub , faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = ({reff}) => {
     return <div className={classes.contact_body} ref={reff}>
     <footer>
         <div> 
-                <a href="https://github.com/rmc3408" ><FontAwesomeIcon icon={faGithub} className={classes.icons} /></a>
-                <a href='https://www.linkedin.com/in/raphael-molinaro-68388b133/'><FontAwesomeIcon icon={faLinkedin} className={classes.icons} /> </a>      
+            <a href="mailto:rmc3408@protonmail.com" ><FontAwesomeIcon icon={faEnvelope} className={classes.icons} /></a>  
+            <a href="https://github.com/rmc3408" ><FontAwesomeIcon icon={faGithub} className={classes.icons} /></a>
+            <a href='https://www.linkedin.com/in/raphael-molinaro-68388b133/'><FontAwesomeIcon icon={faLinkedin} className={classes.icons} /> </a>      
         </div>
          
     <p> Created by Raphael Molinaro @ 2020 </p>
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,10 +1,10 @@
-import React,{ useRef } from "react";
+import React from "react";
 import classes from "./navbar.module.css";
 
 const Navbar = ({toAbout,toProjects,toContact,toHome}) => {
     const arr = [classes.active, classes.list_item];
-    const links = useRef();
-  return <nav className={classes.main_nav}>
+    //const links = useRef();
+  return <nav className={ classes.main_nav }>
       <ul className={classes.nav_items}>
           <li className={arr.join(" ")} onClick={toHome}> Home </li>
           <li className={classes.list_item} onClick={toAbout}> About</li>
