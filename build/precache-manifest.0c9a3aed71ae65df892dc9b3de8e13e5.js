014575ed574fab18836098984fa097e858ec7e16
rmc3408 <rmolina9@my.centennialcollege.ca>
2020-10-14 17:06:43 -0400

    build

---
+++ build/precache-manifest.0c9a3aed71ae65df892dc9b3de8e13e5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ad5140c95e3ac440bbe98efd708fd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16055e1723bfa92feb09",
+    "url": "/static/css/2.efea4837.chunk.css"
+  },
+  {
+    "revision": "d7ee50a976518198daed",
+    "url": "/static/css/main.530b7647.chunk.css"
+  },
+  {
+    "revision": "16055e1723bfa92feb09",
+    "url": "/static/js/2.d51348c3.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.d51348c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ee50a976518198daed",
+    "url": "/static/js/main.015d4bc9.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "546106b00f511118bfdc64dbc100ab8c",
+    "url": "/static/media/Slide3.546106b0.PNG"
+  },
+  {
+    "revision": "3d284152642d93b57e2ede084616a172",
+    "url": "/static/media/Slide4.3d284152.PNG"
+  },
+  {
+    "revision": "7e02c27a99e21f5ee683d42de3c53349",
+    "url": "/static/media/Slide5.7e02c27a.PNG"
+  },
+  {
+    "revision": "ecf24f427285be8dac54cf0915222c6d",
+    "url": "/static/media/background_top2.ecf24f42.jpg"
+  }
+]);
